aab4b5814ae0d39636388b0bd77d1e854b94fb3e
Huynh Minh Thuan <hmtminhthuan@gmail.com>
2023-09-28 16:46:24 +0700

    add bill of landing dto

---
+++ src/DeliveryVHGP.Core/Models/BillOfLandingDto.cs
@@ -0,0 +1,22 @@
+﻿namespace DeliveryVHGP.Core.Models
+{
+    public class BillOfLandingDto
+    {
+        // public string Id { get; set; } = null!;
+        public string? PhoneNumber { get; set; }
+        public double? Total { get; set; }
+        // public string? StoreId { get; set; }
+        public string? BuildingId { get; set; }
+        public string? Note { get; set; }
+        public string? FullName { get; set; }
+        // public double? ShipCost { get; set; }
+        public string? DeliveryTimeId { get; set; }
+        // public string? ServiceId { get; set; }
+        //public string? ModeId { get; set; }
+
+        //public List<BillProductDetailDto> ProductDetail { get; set; }
+        //public List<PaymentDto> Payments { get; set; }
+
+
+    }
+}
